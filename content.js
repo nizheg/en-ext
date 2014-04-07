bf841453842e4a6ac8de332a566509d161c58bc6
Nikolay Zhegalin <nzhegalin@gmail.com>
2014-04-07 23:21:59 +0400

    fix for previous commit

--- content.js
+++ content.js
@@ -12,11 +12,14 @@ chrome.runtime.onMessage.addListener(
 	);
 
 $(function() {
-	var title = $('.content h2:first').text().trim();
-	var sectors_info = $('.content h3[class!=timer]:first').text().trim();
-	var sectors = $('.content .cols-wrapper').html();
+	var title = $('div.content h2:first').text().trim();
+	var sectors_info = $('div.content h3[class!=timer]:first').text().trim();
+	var sectors = $('div.content .cols-wrapper').html();
 	var corrects = $('ul.history li.correct:lt(1)').html();
-	chrome.runtime.sendMessage({"title" : title, "sectors_info" : sectors_info, "sectors" : sectors, "corrects": corrects}, 
+	var done_bonus = $('div.content h3.color_correct').size();
+	var bonus = $('div.content h3.color_bonus').size();
+	chrome.runtime.sendMessage({"title" : title, "sectors_info" : sectors_info, "sectors" : sectors, "corrects": corrects,
+		"done_bonus_count" : done_bonus, "bonus_count" : bonus}, 
 		function(response) {}
 	);
 });
